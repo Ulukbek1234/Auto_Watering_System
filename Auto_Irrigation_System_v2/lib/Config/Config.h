1c2b83b5bf34a1681db448107c9567255985281b
Ulukbek1234 <ulukbek.egemnazarov@gmail.com>
2026-01-25 17:18:39 +0100

    deactivated debug print

--- Auto_Irrigation_System_v2/lib/Config/Config.h
+++ Auto_Irrigation_System_v2/lib/Config/Config.h
@@ -2,7 +2,7 @@
 #define CONFIG_H
 #pragma once
 
-#define DEBUG 1
+#define DEBUG 0
 
 #define DEBUG_PRINT(x)    do { if (DEBUG) Serial.print(x); } while (0)
 #define DEBUG_PRINTLN(x)  do { if (DEBUG) Serial.println(x); } while (0)
