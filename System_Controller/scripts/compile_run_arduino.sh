1643b8e05c921689162363e68a2d8e2ed25bd9f6
Ulukbek1234 <ulukbek.egemnazarov@gmail.com>
2026-01-25 16:47:14 +0100

    path

---
+++ System_Controller/scripts/compile_run_arduino.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+PROJECT_DIR="/home/ulukpi/Desktop/Auto_Watering_System/Auto_Irrigation_System_v2/"
+
+cd "$PROJECT_DIR" || { echo "Directory not found!"; exit 1; }
+
+# Activate PlatformIO official virtual environment
+source ~/.platformio/penv/bin/activate || { echo "PlatformIO venv not found!"; exit 1; }
+
+# Compile
+echo "Compiling the project..."
+pio run || { echo "Compilation failed!"; exit 1; }
+
+# Upload
+echo "Uploading the firmware..."
+pio run --target upload || { echo "Upload failed!"; exit 1; }
+
+echo "Done!"
