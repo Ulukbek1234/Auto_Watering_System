a2a6b404122196cf4b9d9d8446f76ce997011aca
Ulukbek Egemnazarov <ulukbek.egemnazarov@student.tugraz.at>
2026-03-24 20:14:22 +0100

    add pin definitions

--- ESP/AIS_ESP32/src/main.cpp
+++ ESP/AIS_ESP32/src/main.cpp
@@ -7,11 +7,19 @@
 
 #define LED 2
 
+// ---- PINS ----
+// GPIO 32-35: Moisture Sensors Input
+#define MSI_0 32
+// GPIO 16-19: Pump Control Output
+#define PCO_0 16
+
 void setup() {
   // Initialize serial communication
   Serial.begin(115200);
   // Set LED pin as output
   pinMode(LED, OUTPUT);
+  pinMode(MSI_0, INPUT);
+  pinMode(PCO_0, OUTPUT);
 }
 
 void loop() {
@@ -23,4 +31,15 @@ void loop() {
   digitalWrite(LED, LOW);
   Serial.println("LED is off");
   delay(1000);
+
+  uint16_t anal_input = analogRead(MSI_0);
+  Serial.print("Anal_Input: ");
+  Serial.println(anal_input);
+  delay(1000);
+
+  digitalWrite(PCO_0, HIGH);
+  delay(1000);
+  digitalWrite(PCO_0, LOW);
+  delay(1000);
+
 }   
