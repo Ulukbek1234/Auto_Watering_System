9a8db5da5829efe6fe6914893e3e5e521e4e65f7
Ulukbek1234 <ulukbek.egemnazarov@gmail.com>
2026-04-11 13:35:11 +0200

    safe main

--- Auto_Irrigation_System_v2/src/main.cpp
+++ Auto_Irrigation_System_v2/src/main.cpp
@@ -18,9 +18,8 @@ Preferences prefs;
 
 
 Zone *pots;
-Zone *auto_pot;
-int nr_zones = 2; // TODO change dynamically
-Zone *all_pots[2]; // TODO dynamic array if more pots needed in future
+int nr_zones = 1; // TODO change dynamically
+Zone *all_pots[1]; // TODO dynamic array if more pots needed in future
 unsigned long start_time = 0;
 const unsigned long wait_time = 60000; // 1 minute
 // EEPROM 
@@ -75,10 +74,9 @@ void setup() {
   pots->addSoilSensor(A0, eeprom_data.cali_air[0], eeprom_data.cali_water[0]);
   pots->addSoilSensor(A1, eeprom_data.cali_air[1], eeprom_data.cali_water[1]);
   pots->addSoilSensor(A2, eeprom_data.cali_air[2], eeprom_data.cali_water[2]);
-  pots->addSoilSensor(A3, eeprom_data.cali_air[3], eeprom_data.cali_water[3]);
+  pots->addSoilSensor(A4, eeprom_data.cali_air[3], eeprom_data.cali_water[3]);
   
   all_pots[0] = pots;
-  all_pots[1] = auto_pot;
   start_time = millis();
 }
 
